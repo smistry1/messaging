2d104fba891087ffe92931b104cef32df0e9411b
Gibbonator <mattgibbons07@gmail.com>
2015-03-20 12:13:28 +0000

    Added message sender class
    Sends a message with no callbacks

---
+++ src/main/java/net/sanish/sms/MessageSender.java
@@ -0,0 +1,22 @@
+package net.sanish.sms;
+
+import android.content.Context;
+import android.telephony.SmsManager;
+
+public class MessageSender {
+
+    private Context c;
+    private SmsManager manager;
+
+    public MessageSender(Context c) {
+        this.c = c;
+        manager = SmsManager.getDefault();
+    }
+
+    public void send(String messageText, String to) {
+        manager.sendTextMessage(to, null, messageText, null, null) ;
+        // send the message using default service center and no callback functions.
+
+    }
+
+}
